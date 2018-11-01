a12a4566751f0d5dd20741551e4c55645a960a1e
Andreas Eriksson <andreas_e@telia.com>
2018-11-01 17:00:50 +0100

    Shooting Enemy

---
+++ Assets/Scripts/Enemy/ShootingEnemy.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingEnemy : BasicEnemy
+{
+	public GameObject projectile;
+	private Rigidbody2D rbody2d;
+	private GameObject clone;
+
+	public float fireSpeed;
+	private bool canShoot;
+	public float cooldown;
+	private float waitTimer;
+
+	void Start()
+	{
+		speed = 1;
+		hp = 1;
+		damage = 1;
+		score = 30;
+
+		rbody2d = GetComponent<Rigidbody2D>();
+	}
+
+	private void FixedUpdate()
+	{
+		if (canShoot)
+		{
+			Shoot();
+			canShoot = false;
+		}
+
+		if (canShoot == false)
+		{
+			waitTimer += Time.deltaTime;
+
+			if (waitTimer >= cooldown)
+			{
+				canShoot = true;
+				waitTimer = 0f;
+			}
+		}
+	}
+
+	private void Shoot()
+	{
+		clone = (GameObject)Instantiate(projectile, rbody2d.transform.position, Quaternion.identity);
+		clone.transform.Rotate(Vector3.forward * 90f);
+		clone.GetComponent<Rigidbody2D>().velocity = projectile.transform.up * fireSpeed;
+	}
+}
--- Assets/Scripts/LaserDestroy.cs
+++ Assets/Scripts/LaserDestroy.cs
@@ -6,9 +6,18 @@ public class LaserDestroy : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D coll)
     {
-        if (coll.tag != "Player")
+        if (coll.tag != "Player" && gameObject.tag == "Laser")
         {
             Destroy(gameObject);
         }
+		else if (coll.tag != "Enemy" && gameObject.tag == "ELaser")
+		{
+			if (coll.tag == "Player")
+			{
+				coll.gameObject.GetComponent<PlayerHealth>().TakeDamage(1);
+			}
+
+			Destroy(gameObject);
+		}
     }
 }
