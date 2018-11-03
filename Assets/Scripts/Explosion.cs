56f7f06f5f630e5d8209b116b60955c5c1dae05d
Andreas Eriksson <andreas_e@telia.com>
2018-11-03 20:15:55 +0100

    Hit reg + Custom cursor

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+	public float speed;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	void FixedUpdate()
+	{
+		transform.Translate(-speed * Time.deltaTime, 0, 0);
+	}
+}
--- Assets/Scripts/LaserDestroy.cs
+++ Assets/Scripts/LaserDestroy.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class LaserDestroy : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D coll)
+	public Sprite[] hit1;
+	public GameObject explosionMark;
+	private Rigidbody2D rbody2D;
+
+	private void Start()
+	{
+		rbody2D = GetComponent<Rigidbody2D>();
+	}
+
+	private void OnTriggerEnter2D(Collider2D coll)
     {
 		if (coll.tag != "XP")
 		{
 			if (coll.tag != "Player" && gameObject.tag == "Laser")
 			{
+				if (coll.tag == "Enemy")
+				{
+					Explosion(coll.gameObject);
+				}
+
 				Destroy(gameObject);
 			}
 			else if (coll.tag != "Enemy" && gameObject.tag == "ELaser")
@@ -23,4 +37,16 @@ public class LaserDestroy : MonoBehaviour
 			}
 		}
     }
+
+	private void Explosion(GameObject enemy)
+	{
+		GameObject clone = Instantiate(explosionMark, rbody2D.position, Quaternion.identity);
+
+		float spd = enemy.gameObject.GetComponent<BasicEnemy>().speed;
+		clone.gameObject.GetComponent<Explosion>().speed = spd;
+
+		clone.GetComponent<SpriteRenderer>().sprite = hit1[CharacterSelect.arrayPos];
+
+		Destroy(clone, 0.5f);
+	}
 }
