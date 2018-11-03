2d1e425665f54c46d9d9971672f9c9dcb6ba0982
Andreas Eriksson <andreas_e@telia.com>
2018-11-03 11:48:52 +0100

    Select character scene + selector

---
+++ Assets/Scripts/CharacterSelect.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelect : MonoBehaviour
+{
+	public Sprite[] spaceships;
+
+	public int arrayPos;
+
+	public void ButtonNextShip(bool isRight)
+	{
+		if (isRight)
+		{
+			if (arrayPos == spaceships.Length - 1)
+			{
+				arrayPos = 0;
+			}
+			else
+			{
+				arrayPos++;
+			}
+		}
+		else
+		{
+			if (arrayPos == 0)
+			{
+				arrayPos = spaceships.Length - 1;
+			}
+			else
+			{
+				arrayPos--;
+			}
+		}
+
+		gameObject.GetComponent<SpriteRenderer>().sprite = spaceships[arrayPos];
+	}
+}
