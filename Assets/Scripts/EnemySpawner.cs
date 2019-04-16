7da47329af248bcd61c0b21ffec2779e356882a7
Andy01E <andreas.eriksson@elev.ga.lbs.se>
2019-04-16 15:14:41 +0200

    Faster over time gameplay

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    private Rigidbody2D rbody2D;
+    private float enemySpeedMult;
+
 	public GameObject[] enemies;
 
 	public int level;
@@ -11,13 +14,12 @@ public class EnemySpawner : MonoBehaviour
 	public float spawnRate;		//Ifall jag skulle vilja ändra hur snabbt fienderna ska spawna.
 	public float cooldown;
 
-	private Rigidbody2D rbody2D;
-
 	private void Start()
 	{
 		spawnRate = 60f;		//1 fiende i sekunden.
+        enemySpeedMult = 1;
 
-		rbody2D = GetComponent<Rigidbody2D>();
+        rbody2D = GetComponent<Rigidbody2D>();
 	}
 
 	void FixedUpdate()
@@ -27,8 +29,10 @@ public class EnemySpawner : MonoBehaviour
 			GameObject enemyPrefab = enemies[Random.Range(0, level)];
 			float maxSpawnPoint = enemyPrefab.GetComponent<BasicEnemy>().maxSpawnPoint;
 			float minSpawnPoint = enemyPrefab.GetComponent<BasicEnemy>().minSpawnPoint;
-			Instantiate(enemyPrefab, new Vector2(rbody2D.position.x, rbody2D.position.y + Random.Range(maxSpawnPoint, minSpawnPoint)), Quaternion.Euler(0,0,-90f));
+			GameObject enemy = Instantiate(enemyPrefab, new Vector2(rbody2D.position.x, rbody2D.position.y + Random.Range(maxSpawnPoint, minSpawnPoint)), Quaternion.Euler(0,0,-90f));
 			cooldown = 60f / spawnRate;		//60 / 60 = 1
+
+            enemy.GetComponent<BasicEnemy>().speed *= enemySpeedMult;
 		}
 		else
 		{
@@ -41,26 +45,32 @@ public class EnemySpawner : MonoBehaviour
 		if (score < 100)
 		{
 			level = 1;
+            enemySpeedMult = 1;
 		}
 		else if (score < 200)
 		{
 			level = 2;
-		}
+            enemySpeedMult = 1.2f;
+        }
 		else if (score < 400)
 		{
 			level = 3;
-		}
+            enemySpeedMult = 1.5f;
+        }
 		else if (score < 600)
 		{
 			level = 4;
-		}
+            enemySpeedMult = 2;
+        }
 		else if (score < 800)
 		{
 			level = 5;
-		}
+            enemySpeedMult = 2.5f;
+        }
 		else if (score < 1000)
 		{
 			level = 6;
-		}
+            enemySpeedMult = 3;
+        }
 	}
 }
